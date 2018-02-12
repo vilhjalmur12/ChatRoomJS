c9c7a108203910e58143cffc16ab143622ba01c8
vilhjalmur <vilhjalmur12@ru.is>
2018-02-12 23:13:37 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/site';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div class="tester">I am the app</div>
+        );
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
