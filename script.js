569c364e45f6193c710d2fd2325557411764ecd1
vilhjalmur <vilhjalmur12@ru.is>
2018-02-16 23:38:59 +0000

    added bootstrap removed semantic UI

---
+++ script.js
@@ -0,0 +1,38 @@
+// Modal view
+$('#add-room').click( function() {
+        $('.modal').modal('show');
+});
+
+// room sidebar hover
+$('.room-item').hover( function(e) {
+    $(this).css('background', '#3F295C');
+    var desc = $(this).find('.description');
+    desc.removeClass('hidden');
+}, function(e) {
+    $(this).css('background', 'none');
+    $(this).find('.description').addClass('hidden');
+});
+
+// Left menu bar
+$('.left-menu-button').click( function(e) {
+    $('.left-menu-button').removeClass('active');
+    $(this).addClass('active');
+});
+
+// Right menu bar
+$('.right-menu-button').click( function(e) {
+    
+});
+
+$('.right-menu-button').hover( function(e) {
+    $(this).addClass('active');
+}, function(e) {
+    $(this).removeClass('active');
+});
+
+//Byrja scrollable á botni
+$('.ui.raised.segment').scrollTop($('.ui.raised.segment')[0].scrollHeight);
+
+$('.room-drop-button').click( function() {
+    $(this).dropdown();
+});
--- src/app.js
+++ src/app.js
@@ -8,7 +8,29 @@ class App extends React.Component {
     }
     render() {
         return (
-            <div class="tester">I am the appster</div>
+            <div class="ui middle aligned center aligned grid">
+            <div class="column login-column">
+                <h2 class="ui teal image header">
+                    <img src="assets/images/logo.png" class="image"></img>
+                    <div class="content">
+                        Log-in to chat
+                    </div>
+                </h2>
+                <form class="ui large form">
+                    <div class="ui stacked segment">
+                        <div class="field">
+                            <div class="ui left icon input">
+                                <i class="user icon"></i>
+                                <input type="text" name="email" placeholder="Username"></input>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                            </div>
+                        </div>
+                        <div class="ui fluid large teal submit button">Login</div>
+                    </div>
+                    <div class="ui error message"></div>
+                </form>
+            </div>
+        </div>
         );
     }
 }
