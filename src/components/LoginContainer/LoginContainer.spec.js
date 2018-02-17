078491721e1935b16aeaf047c49cef0a68bf3732
Vilhjálmur Rúnar Vilhjálmsson <vilhjalmur12@ru.is>
2018-02-17 02:14:19 +0000

    jest is ready to go!

--- src/components/LoginContainer/LoginContainer.spec.js
+++ src/components/LoginContainer/LoginContainer.spec.js
@@ -1,6 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LoginContainer from './LoginContainer';
+
 describe('Test login_container', () => {
+    let comp;
+    beforeEach(() => {
+        comp = shallow(<LoginContainer />);
+    });
+
     it('should contain a div with container class', () => {
-        const comp = shallow(<LoginContainer />);
-        expect(comp.contains(<div className="container">hello</div>)).toBe(true);
+
+        expect(comp.contains(<div className="container"></div>)).toBe(true);
     });
 });
